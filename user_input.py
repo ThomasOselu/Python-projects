e4f8f7f28cb5f07aedc97aafc365ee2bcba6735c
ThomasOselu <thomasoselu1@gmai.com>
2024-04-04 11:50:16 +0300

    python basics

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input ('Please enter your name:')
+age = input ('Please enter your age:')
+location = input ('Enter your location:')
+
+print ('Hello {}, you are {} years old and live in {}'.format(name, age, location))
